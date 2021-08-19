dede2ff360dc732e46451d16a526392a0f5ebbf4
Luiz Eduardo Kauan da Silva <luiz.trindade2002@gmail.com>
2021-08-19 07:09:28 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_01/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Dim nome As String
+        Dim idade As Integer
+        Dim salario As Double
+        Dim verdadeiro As Boolean
+
+        nome = "Luiz Eduardo"
+        idade = 18
+        salario = 600.0
+        verdadeiro = True
+
+        Console.WriteLine(nome)
+        Console.WriteLine(idade)
+        Console.WriteLine(salario)
+        Console.WriteLine(verdadeiro)
+
+    End Sub
+End Module
